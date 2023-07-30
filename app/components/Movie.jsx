5311a852658c7ef458415a891485e0cf21996290
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-07-30 16:49:20 +0100

    Data rendered inside Movie component

--- app/components/Movie.jsx
+++ app/components/Movie.jsx
@@ -1,12 +1,17 @@
 import Link from "next/link"
 
 
-const Movie = ({ title }) => {
+const Movie = ({ title, id, poster_path, release_date }) => {
+
+  const imagePath = "https://image.tmdb.org/t/p/w500/"
+
   return (
     <div>
         <h1>{title}</h1>
-        <h2>Release Date</h2>
-        <Link href={`/#`}>Details</Link>
+        <h2>{release_date}</h2>
+        <Link href={`/#`}>
+          <img src={imagePath + poster_path} alt={title} />
+        </Link>
     </div>
   )
 }
