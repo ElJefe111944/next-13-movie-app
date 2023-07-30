d69311d97a5cb3fe48b31444c6541ba7c720a94f
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-07-30 18:17:58 +0100

    Movie detail data rendered in component

--- app/[id]/page.jsx
+++ app/[id]/page.jsx
@@ -1,9 +1,35 @@
+import Image from "next/image";
+
+export default async function MovieDetails({ params }) {
 
 
-const MovieDetail = () => {
-  return (
-    <div>MovieDetail</div>
-  )
+    const imagePath = "https://image.tmdb.org/t/p/w500/"
+
+    const Id = params.id
+
+    const res = await fetch(`https://api.themoviedb.org/3/movie/${Id}?api_key=${process.env.API_KEY}`, {
+        next: { revalidate: 60 }
+    });
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch data')
+    }
+
+    const data = await res.json();
+
+
+    return (
+        <div>
+            <div>
+                <h2 className="text-2xl">{data.title}</h2>
+                <h2 className="text-lg">{data.release_date}</h2>
+                <h2>Runtime: {data.runtime} minutes</h2>
+                <h2 className="bg-green-600 inline-block my-2 py-2 px-2 text-white rounded-md text-sm">{data.status}</h2>
+                <Image width={1000} height={1000} className="my-12 w-full" src={imagePath + data.backdrop_path} priority />
+                <p>{data.overview}</p>
+            </div>
+        </div>
+    )
+
 }
 
-export default MovieDetail
